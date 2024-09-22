d489bf2a980ef8eda52b7531daefba7566f8d6a7
ZachBeaupre <zach511nfs@outlook.com>
2024-09-21 20:29:23 -0400

    added some comments made keys always look at the player, because I finally figured out why atan2 NEVER WORKED turns out that atan2's parameters have y come before x, which makes me very mad and angy >=^( using this I can finally add sprites maybe
    Corrected a typo in a comment.

--- src/masterkey.java
+++ src/masterkey.java
@@ -33,13 +33,16 @@ public class masterkey
         if(!used) {
         final double halfPi = Math.PI / 2;
 
-        double theta = Math.atan2(player.getPos().getY()-y,player.getPos().getY()-x); //gets the angle from the players position to the key. Making it always face you, creepy but cool
+
+            double theta = Math.atan2(player.getPos().getY()-y,player.getPos().getX()-x); //gets the angle from the players position to the key. Making it always face you, creepy but cool
             //I do intend to use this to make sprites and cokes work. maybe just sprites.
             //it reminds me of gmod nextbots or the people in crowds in forza horizon 4 or just old raycasters
+            //i accidentally messed up order of x and y again lol
+            
 
 
 
-            return new Line2D.Double(x + 2.5 * Math.cos(theta+halfPi), y + 2.5 * Math.sin(theta+halfPi), x, y);
+            return new Line2D.Double(x + 5 * Math.cos(theta+halfPi), y + 5 * Math.sin(theta+halfPi), x, y);
 //            return new Line2D.Double(x - 2.5, y - 2.5, x, y); //my old method of making the keys. Before 9/20/2024 I didnt know that the Math.atan2() functions parameters are ( y, x ) instead of ( x, y ). It is the stupidest thing ever and I hate it with every fibre and fiber of my being.
         }else{
             return new Line2D.Double(1000,1000,1000,1000);
